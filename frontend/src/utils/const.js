a0cb927252f077ed20eb411a134a83d51e1aa5aa
Saumya-Aggarwal <saumyaaggarwal667@gmail.com>
2025-04-20 00:11:50 +0530

    url issue

--- frontend/src/utils/const.js
+++ frontend/src/utils/const.js
@@ -1,5 +1,8 @@
-// Update these to use relative URLs instead of localhost
-export const USER_API_END_POINT = "/user";
-export const JOB_API_END_POINT = "/job";
-export const APPLICATION_API_END_POINT = "/application";
-export const COMPANY_API_END_POINT = "/company";
+// Use absolute URLs in production, relative URLs in development
+const isProduction = window.location.hostname !== 'localhost';
+const API_BASE = isProduction ? 'https://job-portal-beta-one.vercel.app' : '';
+
+export const JOB_API_END_POINT = `${API_BASE}/job`;
+export const APPLICATION_API_END_POINT = `${API_BASE}/application`;
+export const USER_API_END_POINT = `${API_BASE}/user`;
+export const COMPANY_API_END_POINT = `${API_BASE}/company`;

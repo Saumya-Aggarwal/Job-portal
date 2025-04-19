947eba9bf4abf489c860bd2cf09f4830eb50d39d
Saumya-Aggarwal <saumyaaggarwal667@gmail.com>
2025-04-20 00:37:18 +0530

    url correct

--- frontend/src/utils/const.js
+++ frontend/src/utils/const.js
@@ -1,6 +1,6 @@
 // Use absolute URLs in production, relative URLs in development
 
-const API_BASE = 'https://job-portal-08im.onrender.com/'
+const API_BASE = 'https://job-portal-08im.onrender.com'
 
 export const JOB_API_END_POINT = `${API_BASE}/job`;
 export const APPLICATION_API_END_POINT = `${API_BASE}/application`;
